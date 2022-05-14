1c47fcd723150bbff2c3ad6746e65b82711a66cb
Antonio <jopez1@hotmail.com>
2022-05-14 17:15:45 -0500

    Homework HTML Listo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -197,7 +197,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola ${Martin} !'
+  return "Hola "+nombre+"!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
